d637ad835ef70e8b85057047e34af9d4aa678670
Juliano <kluge233@hotmail.com>
2023-03-05 10:42:54 -0300

    Created Conatiner with status

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,12 +1,177 @@
+import 'package:booking/utils/app_layout.dart';
+import 'package:booking/utils/app_styles.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class AccountScreen extends StatelessWidget {
   const AccountScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.yellow,
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+            horizontal: AppLayout.getHeight(20),
+            vertical: AppLayout.getWith(20)),
+        children: [
+          Gap(AppLayout.getHeight(40)),
+          Row(
+            children: [
+              Container(
+                height: AppLayout.getHeight(86),
+                width: AppLayout.getWith(86),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(AppLayout.getHeight(10)),
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/img_1.png')),
+                ),
+              ),
+              Gap(AppLayout.getHeight(10)),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Book tickets',
+                    style: Styles.headLineStyle,
+                  ),
+                  Gap(AppLayout.getHeight(2)),
+                  Text(
+                    'New York',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey.shade500,
+                    ),
+                  ),
+                  Gap(AppLayout.getHeight(8)),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.circular(AppLayout.getHeight(100)),
+                      color: const Color(0xFFFEF4F3),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                        horizontal: AppLayout.getHeight(3),
+                        vertical: AppLayout.getHeight(3)),
+                    child: Row(children: [
+                      Container(
+                        padding: EdgeInsets.all(3),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: const Color(0xFF526799),
+                        ),
+                        child: Icon(
+                          FluentSystemIcons.ic_fluent_shield_filled,
+                          color: Colors.white,
+                          size: 15,
+                        ),
+                      ),
+                      Gap(
+                        AppLayout.getHeight(3),
+                      ),
+                      const Text(
+                        'Premium status',
+                        style: TextStyle(
+                          color: const Color(0xFF526799),
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Gap(AppLayout.getHeight(5))
+                    ]),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              Column(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      print('Vc clicou');
+                    },
+                    child: Text('Edit',
+                        style: Styles.textStyle.copyWith(
+                          color: Styles.primaryColor,
+                          fontWeight: FontWeight.w300,
+                        )),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Gap(AppLayout.getHeight(8)),
+          Divider(
+            color: Colors.grey.shade500,
+          ),
+          Gap(AppLayout.getHeight(8)),
+          Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                height: AppLayout.getHeight(90),
+                decoration: BoxDecoration(
+                  color: Styles.primaryColor,
+                  borderRadius: BorderRadius.circular(AppLayout.getHeight(18)),
+                ),
+              ),
+              Positioned(
+                right: -45,
+                top: -40,
+                child: Container(
+                  padding: EdgeInsets.all(AppLayout.getHeight(30)),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(width: 18, color: Color(0xFF264CD2)),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                    horizontal: AppLayout.getHeight(20),
+                    vertical: AppLayout.getHeight(20)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      maxRadius: 25,
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        FluentSystemIcons.ic_fluent_lightbulb_filament_filled,
+                        color: Styles.primaryColor,
+                        size: 27,
+                      ),
+                    ),
+                    Gap(AppLayout.getHeight(8)),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'You have a got new ticket!',
+                          style: Styles.headLineStyle3.copyWith(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        ),
+                        Text(
+                          'You have a 84 flights in year',
+                          style: Styles.headLineStyle3.copyWith(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 16),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
